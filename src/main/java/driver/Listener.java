65fa62fd4063b16cbe8ab4d86e1bf4eea940baa5
Vitali Shadrin <vitali.shadrin.consultant@nielsen.com>
2021-09-04 18:48:30 +0300

    Added listener om start

--- src/main/java/driver/Listener.java
+++ src/main/java/driver/Listener.java
@@ -3,8 +3,17 @@ package driver;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+
 import static driver.DriverCreation.*;
 
 
@@ -16,8 +25,24 @@ public class Listener implements ITestListener {
         saveScreenshots(file);
     }
 
+    @Override
+    public void onStart(ITestContext context) {
+        Path path = Paths.get("allure-results");
+        try {
+            Files.walk(path)
+                    .sorted(Comparator.reverseOrder())
+                    .map(Path::toFile)
+                    .forEach(File::delete);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
     @Attachment(value = "Screenshots", type = "image/png")
     private byte[] saveScreenshots(byte[] s){
         return s;
     }
+
+
 }
