dc84faf5d9304f11e7bd003d420ddcb58a25348f
Vitali Shadrin <vitali.shadrin.consultant@nielsen.com>
2021-10-27 10:49:14 +0300

    Update Listener.java

--- src/main/java/testng/Listener.java
+++ src/main/java/testng/Listener.java
@@ -1,5 +1,6 @@
 package testng;
 
+import com.codeborne.selenide.WebDriverRunner;
 import driver.DriverCreation;
 import driver.SelenideConfigurations;
 import io.qameta.allure.Attachment;
@@ -10,21 +11,12 @@ import org.testng.ITestListener;
 import org.testng.ITestResult;
 import properties.PropertyReader;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Comparator;
-
-import static driver.DriverCreation.getDriver;
-
 
 public class Listener implements ITestListener {
 
     @Override
     public void onTestFailure(ITestResult result) {
-        byte[] file = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.BYTES);
+        byte[] file = ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
         saveScreenshots(file);
     }
 
@@ -33,31 +25,10 @@ public class Listener implements ITestListener {
         PropertyReader propertyReader = new PropertyReader();
         propertyReader.setProperties(context.getSuite().getParameter("env") == null ? System.getProperties().getProperty("env") : context.getSuite().getParameter("env"));
         new SelenideConfigurations(propertyReader);
-        clearTestsResults();
-    }
-
-    @Override
-    public void onFinish(ITestContext context) {
-        DriverCreation.quitDriver();
     }
 
     @Attachment(value = "Screenshots", type = "image/png")
     private byte[] saveScreenshots(byte[] s) {
         return s;
-    }
-
-
-    private void clearTestsResults() {
-        Path path = Paths.get("allure-results");
-        try {
-            if (Files.exists(path)) {
-                Files.walk(path)
-                        .sorted(Comparator.reverseOrder())
-                        .map(Path::toFile)
-                        .forEach(File::delete);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
